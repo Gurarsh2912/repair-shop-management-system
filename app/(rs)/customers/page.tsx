04e4a6c4002a18ed6317b3a3b7e21d8af2d12d42
Gurarsh <gurarshdeep7@gmail.com>
2026-08-10 23:54:13 +0530

    fetch customer and ticket data

--- app/(rs)/customers/page.tsx
+++ app/(rs)/customers/page.tsx
@@ -1,11 +1,24 @@
+import CustomerSearch from "@/app/(rs)/customers/CustomerSearch"
+import { getCustomerSearchResults } from "@/lib/queries/getCustomersSearchResults"
+
 export const metadata = {
-    title:  "Customers", 
+    title:  "Customer Search", 
 }
 
-export default function Customers(){
-    return(
-        <h2>
-            CustomersPage
-        </h2>
+export default async function Customers({
+    searchParams,
+}:{
+    searchParams: Promise<{[key:string]: string | undefined}>
+}){
+    const {searchText} = await searchParams
+    if(!searchText) return <CustomerSearch/>
+
+    const results = await getCustomerSearchResults(searchText)
+
+    return (
+        <>
+            <CustomerSearch/>
+            <p>{JSON.stringify(results)}</p>
+        </>
     )
 }
--- app/(rs)/tickets/page.tsx
+++ app/(rs)/tickets/page.tsx
@@ -1,11 +1,34 @@
+import TicketSearch from "./TicketSearch"
+import { getOpenTickets } from "@/lib/queries/getOpenTickets"
+import { getTicketSearchResults } from "@/lib/queries/getTicketSearchResult"
+
 export const metadata = {
-    title:  "Tickets", 
+    title:  "Ticket Search", 
 }
 
-export default function Tickets(){
-    return(
-        <h2>
-            TicketsPage
-        </h2>
+export default async function Tickets({
+    searchParams,
+}:{
+    searchParams: Promise<{[key:string]: string | undefined}>
+}){
+    const {searchText} = await searchParams
+    if(!searchText){
+        const results = await getOpenTickets();
+        return( //default results
+            <>
+                <TicketSearch/>
+                <p>{JSON.stringify(results)}</p>
+            </>
+        )
+    }
+
+    const results = await getTicketSearchResults(searchText)
+
+    return(  //search using params
+        <>
+            <TicketSearch/>
+            <p>{JSON.stringify(results)}</p>
+        </>
     )
+    
 }
